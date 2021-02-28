23047f726a88ee7151cac5ea5fbb0e28a697b348
Yasutaka Kawamoto <kwmt27@gmail.com>
2021-02-28 17:34:51 +0900

    fetch data from json file with di

--- app/src/main/java/com/example/androiddevchallenge/application/di/ViewModelModule.kt
+++ app/src/main/java/com/example/androiddevchallenge/application/di/ViewModelModule.kt
@@ -1,22 +1,40 @@
 package com.example.androiddevchallenge.application.di
 
+import android.content.Context
 import com.example.androiddevchallenge.domain.repository.AnimalRepository
 import com.example.androiddevchallenge.infrastructure.AnimalDataSource
 import com.example.androiddevchallenge.infrastructure.AnimalLocalDataSource
 import com.example.androiddevchallenge.infrastructure.AnimalRemoteDataSource
+import com.example.androiddevchallenge.infrastructure.Api
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.android.scopes.ViewModelScoped
+import dagger.hilt.components.SingletonComponent
 
 @Module
 @InstallIn(ViewModelComponent::class)
 object ViewModelModule {
     @Provides
     @ViewModelScoped
-    fun provideRepository(): AnimalRepository = AnimalDataSource(
-        AnimalRemoteDataSource(),
-        AnimalLocalDataSource()
+    fun provideRepository(
+        remoteDataSource: AnimalRemoteDataSource,
+        localDataSource: AnimalLocalDataSource
+    ): AnimalRepository = AnimalDataSource(
+        remoteDataSource,
+        localDataSource
     )
+}
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DataSourceModule {
+    @Provides
+    fun provideApi(
+        @ApplicationContext context: Context
+    ): Api {
+        return Api(context)
+    }
 }
--- app/src/main/java/com/example/androiddevchallenge/infrastructure/AnimalDataSource.kt
+++ app/src/main/java/com/example/androiddevchallenge/infrastructure/AnimalDataSource.kt
@@ -1,27 +1,54 @@
 package com.example.androiddevchallenge.infrastructure
 
+import android.content.Context
+import android.content.res.AssetManager
 import com.example.androiddevchallenge.domain.model.Animal
 import com.example.androiddevchallenge.domain.repository.AnimalRepository
+import com.example.androiddevchallenge.infrastructure.model.AnimalResponse
+import com.example.androiddevchallenge.infrastructure.model.AnimalsResponse
+import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import javax.inject.Inject
 
-class AnimalDataSource(
+class AnimalDataSource @Inject constructor(
     private val remoteDataSource: AnimalRemoteDataSource,
     private val localDataSource: AnimalLocalDataSource
 ) : AnimalRepository {
     override fun fetchAnimals(): Flow<List<Animal>> {
-        return localDataSource.latestAnimals
+        return remoteDataSource.latestAnimals
+        //localDataSource.latestAnimals
     }
 }
 
-class AnimalRemoteDataSource {
+class AnimalRemoteDataSource @Inject constructor(
+    api: Api
+) {
+    val latestAnimals: Flow<List<Animal>> = api.fetchAnimalData().map { it.toModel() }
 }
 
-class AnimalLocalDataSource {
+class AnimalLocalDataSource @Inject constructor() {
     val latestAnimals: Flow<List<Animal>> = flow {
         emit(createAnimalListData())
     }
 }
+
+class Api @Inject constructor(
+    @ApplicationContext context: Context,
+) {
+    private val assetManager = context.assets
+    fun fetchAnimalData(): Flow<AnimalsResponse> = flow {
+        val json = assetManager.readAssetsFile("mock_data.json")
+        val element = Json.decodeFromString<List<AnimalResponse>>(json)
+        emit(AnimalsResponse(element))
+    }
+}
+
+private fun AssetManager.readAssetsFile(fileName: String): String =
+    open(fileName).bufferedReader().use { it.readText() }
 
 fun createAnimalListData(): List<Animal> {
     return listOf(
